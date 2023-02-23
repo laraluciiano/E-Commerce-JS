37b2abdf32334a85dbdcc625774afa055b33ecb7
laraluciiano <lara.kentros@gmail.com>
2023-02-23 20:10:49 -0300

    arreglos en js

--- script.js
+++ script.js
@@ -82,29 +82,54 @@ if (close) {
     
     function ready(){
     //Quitar items del carrito
-    let removeCartButtons = document.getElementsByClassName('cart-remove')
+    const removeCartButtons = document.getElementsByClassName(".cart-remove")
     console.log(removeCartButtons)
     for (let i = 0; i< removeCartButtons.length; i++){
         let button = removeCartButtons[i]
         button.addEventListener("click", removeCartItem)
     }
 
-    //Quitar items del carrito
     function removeCartItem (event){
-    let buttonClicked = event.target
+    const buttonClicked = event.target
     buttonClicked.parentElement.remove()
         updateTotal();
     }
+
+    //Cantidad cambiada
+    function quantityChanged(event){
+        let input = event.target
+        if (isNaN(input.value) || input.value <=0) {
+            input.value = 1
+        }
+        updateTotal();
+    }
+
+    //Actualizar total
+    function updateTotal(){
+        let cartContent = document.getElementsByClassName(".cart-content")[0]
+        let cartBoxes = cartContent.getElementsByClassName(".cart-box")
+        let total = 0;
+        for (let i = 0; i< cartBoxes.length; i++){
+            let cartBox = cartBoxes[i]
+            let priceElement = cartBox.getElementsByClassName(".cart-price")[0]
+            let quantityElement = cartBox.getElementsByClassName(".cart-quantity")[0]
+            let price = parseFloat (priceElement.innerText.replace("$", ""));
+            let quantity = quantityElement.value
+            total = total + (price * quantity);
+
+            document.getElementsByClassName(".total-price")[0].innerText = '$' + total;
+        }
+    }
     
     //Cambios en cantidad
-    let quantityInputs = document.getElementsByClassName('cart-quantity')
+    let quantityInputs = document.getElementsByClassName(".cart-quantity")
     for (let i = 0; i< quantityInputs.length; i++){
         let input = quantityInputs[i]
         input.addEventListener('change', quantityChanged)
     }
 
     //Agregar al carrito
-    let addCart = document.getElementsByClassName('fa-cart-shopping');
+    let addCart = document.getElementsByClassName('add-cart');
     for (let i = 0; i< addCart.length; i++){
         let button = addCart[i]
         button.addEventListener('click', addCartClicked);
@@ -120,15 +145,6 @@ if (close) {
         let cartContent = document.getElementsByClassName('cart-content')[0]
         while (cartContent.hasChildNodes()){
             cartContent.removeChild(cartContent.firstChild);
-        }
-        updateTotal();
-    }
-    
-    //Cantidad cambiada
-    function quantityChanged(event){
-        let input = event.target
-        if (isNaN(input.value) || input.value <=0) {
-            input.value = 1
         }
         updateTotal();
     }
@@ -170,19 +186,3 @@ if (close) {
     cartShopBox.getElementsByClassName('cart-remove')[0].addEventListener("click", removeCartItem);
     cartShopBox.getElementsByClassName('cart-quantity')[0].addEventListener("change", quantityChanged);
 }
-
-    //Actualizar total
-    function updateTotal(){
-        let cartContent = document.getElementsByClassName('cart-content')[0];
-        let cartBoxes = cartContent.getElementsByClassName('cart-box');
-        let total = 0;
-        for (let i = 0; i< cartBoxes.length; i++){
-            let cartBox = cartBoxes[i];
-            let priceElement = cartBox.getElementsByClassName('cart-price')[0];
-            let quantityElement = cartBox.getElementsByClassName('cart-quantity')[0];
-            let price = parseFloat(priceElement.innerText.replace('$', ""));
-            let quantity = quantityElement.value;
-            total = total + (price * quantity);
-        }
-            document.getElementsByClassName('total-price')[0].innerText = "$" + total;
-    }
